19267f6ec039e66155bb63444922b8eaca82d7ac
jaohenriqu3 <jhlucena2004@gmail.com>
2024-11-25 22:24:21 -0300

    test

---
+++ FinDrive/src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text> 
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
